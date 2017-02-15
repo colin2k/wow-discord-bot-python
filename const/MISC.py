fedda12127fb00e57ce5f83fc909d885e34aeed9
Seryth <stereoleben+wow@gmail.com>
2017-02-15 19:11:50 +0100

    added constants

---
+++ const/MISC.py
@@ -0,0 +1,6 @@
+'''
+Server-Info:
+'''
+LOGGING_CHANNEL = "logs"
+DEFAULT_NICKNAME = "ElyaBot"
+DEFAULT_GAME = "being a bot"
---
+++ const/PATHS.py
@@ -0,0 +1,15 @@
+'''
+Filepaths
+'''
+BASE_PATH = "."
+PROCESS_JSON_PATH = "cmds/replies.json"
+
+'''
+API-Paths
+'''
+BATTLE_NET_API = "https://eu.api.battle.net/wow/character/"
+
+'''
+IPs
+'''
+SIMC_IP = "192.168.100.2"
