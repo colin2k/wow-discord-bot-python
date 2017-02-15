475475805b53d2afa57c8a15e418ef3abce5fdf3
Seryth <stereoleben+wow@gmail.com>
2017-02-15 19:25:15 +0100

    added const for tokens

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,6 +1,6 @@
 from controllers import clientController
 from controllers import commandController
-
+from const import TOKENS
 
 def runClient():
-    clientController.client.run('Mjc1OTk2MTIyODYxMDEwOTQ1.C4WqpA.6lL0BnzEvv4DlquFRNPKufLIh6g')
+    clientController.client.run(TOKENS.DEV_MBL)
